778eb5be7506cc4abf5ef6211ba7ee8f8c25a856
Maxi <mpezzotta@folcode.com>
2023-05-11 03:31:50 -0300

    remove unnecessary  components

--- components/molecules/Header/header.component.tsx
+++ components/molecules/Header/header.component.tsx
@@ -1,23 +1,18 @@
 import Image from "next/image"
 import Link from "next/link"
-import { AppBar, Toolbar, Container } from "@mui/material"
 import Pokemon from "public/images/pokemon.webp"
 import styles from "@/components/molecules/Header/header.module.scss"
 
 export const HeaderComponent = () => (
-  <AppBar position="static" className={styles.nav__header}>
-    <Container maxWidth="xl">
-      <Toolbar>
-        <Link href={"/"}>
-          <Image
-            alt="Pokemon brand"
-            width={150}
-            height={50}
-            src={Pokemon}
-            placeholder="blur"
-          />
-        </Link>
-      </Toolbar>
-    </Container>
-  </AppBar>
+  <header className={styles.nav__header}>
+    <Link href={"/"}>
+      <Image
+        alt="Pokemon brand"
+        width={150}
+        height={50}
+        src={Pokemon}
+        placeholder="blur"
+      />
+    </Link>
+  </header>
 )
